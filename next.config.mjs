41fa48ba15a3f2b3f2b35e1ec5d8a78744487647
Panuwat Chinpratan <panuwatchinpratan@gmail.com>
2025-10-21 17:47:53 +0700

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// @ts-check
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias ?? {}),
-      "redux-toolkit": path.resolve(__dirname, "src/lib/redux-toolkit.ts"),
-    };
-    return config;
-  },
+  /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
